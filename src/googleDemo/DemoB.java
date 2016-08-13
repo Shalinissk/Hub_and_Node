e578189f0c67b8833199e4b498899f5ad992f62c
Shalini S <shalu.ssk@gmail.com>
2016-08-13 18:56:43 +0530

    test

--- src/googleDemo/DemoB.java
+++ src/googleDemo/DemoB.java
@@ -10,22 +10,50 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 import org.testng.xml.XmlTest;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 //If we want to specify browser version, platform and all
 public class DemoB {
 	public WebDriver driver;
+	ExtentReports e = new ExtentReports("./ExtentReports/ExtentReport.html");
 	 @Test
-	 public void DemoGoogle(XmlTest t) throws MalformedURLException
+	 public void DemoGoogle(XmlTest r) throws MalformedURLException
 	 {
-		 String browser = t.getParameter("browser");
-		 String url=t.getParameter("url");
-		 Reporter.log(browser, true);
-		 URL rd = new URL(url);
-		 DesiredCapabilities c = new DesiredCapabilities();
-		 c.setBrowserName(browser);
-		 WebDriver driver = new RemoteWebDriver(rd, c);
-//		 System.setProperty("webdriver.chrome.driver", "D:/RS1/cd.exe/");
-//		 driver = new ChromeDriver();
-		 driver.get("http://www.google.com/");
-		 driver.close();
+		 ExtentTest t1 = e.startTest("DemoGoogle");
+		 t1.log(LogStatus.PASS, "Pass");
+		 e.endTest(t1);
+//		 String browser = r.getParameter("browser");
+//		 String url=r.getParameter("url");
+//		 Reporter.log(browser, true);
+//		 URL rd = new URL(url);
+//		 DesiredCapabilities c = new DesiredCapabilities();
+//		 c.setBrowserName(browser);
+//		 WebDriver driver = new RemoteWebDriver(rd, c);
+////		 System.setProperty("webdriver.chrome.driver", "D:/RS1/cd.exe/");
+////		 driver = new ChromeDriver();
+//		 driver.get("http://www.google.com/");
+//		 driver.close();
+	 }
+	 
+	 @Test
+	 public void DemoFail(XmlTest r) throws MalformedURLException
+	 {
+		 ExtentTest t2 = e.startTest("DemoGoogle");
+		 t2.log(LogStatus.FAIL, "Fail");
+		 e.endTest(t2);
+		 e.flush();
+//		 String browser = r.getParameter("browser");
+//		 String url=r.getParameter("url");
+//		 Reporter.log(browser, true);
+//		 URL rd = new URL(url);
+//		 DesiredCapabilities c = new DesiredCapabilities();
+//		 c.setBrowserName(browser);
+//		 WebDriver driver = new RemoteWebDriver(rd, c);
+////		 System.setProperty("webdriver.chrome.driver", "D:/RS1/cd.exe/");
+////		 driver = new ChromeDriver();
+//		 driver.get("http://www.google.com/");
+//		 driver.close();
 	 }
 	}
--- src/googleDemo/GoogleDemo.java
+++ src/googleDemo/GoogleDemo.java
@@ -11,24 +11,55 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 //If we want to specify simply browser specify this code
 public class GoogleDemo 
 {
 	public WebDriver driver;
+	ExtentReports e = new ExtentReports("./ExtentReports/ExtentReport.html");
  @Test
- public void DemoGoogle() throws MalformedURLException
+ public void DemoA() throws MalformedURLException
  {
-	 URL remoteaddress = new URL("http://10.10.4.61:4444/wd/hub");
-	 DesiredCapabilities dc=new DesiredCapabilities();
-	 dc.setBrowserName("firefox");
-	 dc.setVersion("47");
-	 //dc.setPlatform(Platform.WINDOWS);
-	 WebDriver driver = new RemoteWebDriver(remoteaddress,DesiredCapabilities.firefox());
+	 
+	 ExtentTest t = e.startTest("DemoGoogle");
+	 t.log(LogStatus.PASS, "Hi!!!!!!!!!");
+	 e.endTest(t);
+	 //e.flush();
+//	 URL remoteaddress = new URL("http://10.10.4.61:4444/wd/hub");
+//	 DesiredCapabilities dc=new DesiredCapabilities();
+//	 dc.setBrowserName("firefox");
+//	 dc.setVersion("46");
+//	 //dc.setPlatform(Platform.WINDOWS);
+//	 WebDriver driver = new RemoteWebDriver(remoteaddress,DesiredCapabilities.firefox());
 //	 WebDriver driver = new FirefoxDriver();
 	 
-	 System.setProperty("webdriver.chrome.driver", "D:/RS1/cd.exe/");
-	 driver = new ChromeDriver();
-	 driver.get("http://www.google.com/");
+//	 System.setProperty("webdriver.chrome.driver", "D:/RS1/cd.exe/");
+//	 driver = new ChromeDriver();
+//	 driver.get("http://www.google.com/");
+//	 driver.close();
+ }
+ 
+ @Test
+ public void DemoB() throws MalformedURLException
+ {
+	 ExtentTest t = e.startTest("DemoFail");
+	 t.log(LogStatus.FAIL, "Failed");
+	 e.endTest(t);
+	 e.flush();
+//	 URL remoteaddress = new URL("http://10.10.4.61:4444/wd/hub");
+//	 DesiredCapabilities dc=new DesiredCapabilities();
+//	 dc.setBrowserName("firefox");
+//	 dc.setVersion("46");
+//	 //dc.setPlatform(Platform.WINDOWS);
+//	 WebDriver driver = new RemoteWebDriver(remoteaddress,DesiredCapabilities.firefox());
+////	 WebDriver driver = new FirefoxDriver();
+	 
+//	 System.setProperty("webdriver.chrome.driver", "D:/RS1/cd.exe/");
+//	 driver = new ChromeDriver();
+//	 driver.get("http://www.google.com/");
 //	 driver.close();
  }
 }
